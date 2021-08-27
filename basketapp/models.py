b524f9f7748bf0245d33db2ba4587997f20322c1
MaksShI <76102358+MaksShI@users.noreply.github.com>
2021-08-27 17:36:01 +0300

    Update models.py

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from mainapp.models import Product
+from django.utils.functional import cached_property
 
 
 class Basket(models.Model):
@@ -16,21 +17,17 @@ class Basket(models.Model):
 
     product_cost = property(_get_product_cost)
 
-    def _get_total_quantity(self):
-        "return total quantity for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
-        return _totalquantity
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
 
-    total_quantity = property(_get_total_quantity)
+    def get_total_quantity(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.quantity, _items)))
 
-    def _get_total_cost(self):
-        "return total cost for user"
-        _items = Basket.objects.filter(user=self.user)
-        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
-        return _totalcost
-
-    total_cost = property(_get_total_cost)
+    def get_total_cost(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.product_cost, _items)))
 
     @staticmethod
     def get_items(user):
