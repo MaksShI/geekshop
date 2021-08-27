0726656b8cff333dbdb42e39586d1aabc3d5bfc1
MaksShI <76102358+MaksShI@users.noreply.github.com>
2021-08-27 17:44:11 +0300

    f

---
+++ basketapp/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.conf import settings
+from mainapp.models import Product
+from django.utils.functional import cached_property
+
+
+class Basket(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
+    add_datetime = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
+
+    def _get_product_cost(self):
+        "return cost of all products this type"
+        return self.product.price * self.quantity
+
+    product_cost = property(_get_product_cost)
+
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
+    def get_total_quantity(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.quantity, _items)))
+
+    def get_total_cost(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.product_cost, _items)))
+
+    @staticmethod
+    def get_items(user):
+        return Basket.objects.filter(user=user)
+
+    @staticmethod
+    def get_product(user, product):
+        return Basket.objects.filter(user=user, product=product)
+
+    @staticmethod
+    def get_products_quantity(cls, user):
+        basket_items = cls.get_items(user)
+        basket_items_dic = {}
+        [basket_items_dic.update({item.product: item.quantity}) for item in basket_items]
+
+        return basket_items_dic
+
