359e54de95f70ebeec76c2f11b949b55e48fc68d
MaksShI <76102358+MaksShI@users.noreply.github.com>
2021-08-27 17:57:12 +0300

    f

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -205,3 +205,19 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+
+ENV_TYPE = os.getenv('ENV_TYPE')
+# if os.name == 'posix':
+if ENV_TYPE in ['prod', 'test', 'stage']:
+    CACHE_MIDDLEWARE_ALIAS = 'default'
+    CACHE_MIDDLEWARE_SECONDS = 120
+    CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
+
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+        }
+    }
+
+LOW_CACHE = True
