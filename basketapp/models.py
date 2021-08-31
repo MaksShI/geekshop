f28ca0ce2859447ae63b27e0b0f5f5cfbd7d486b
MaksShI <76102358+MaksShI@users.noreply.github.com>
2021-08-31 18:43:53 +0300

    f

--- basketapp/models.py
+++ basketapp/models.py
@@ -5,14 +5,12 @@ from django.utils.functional import cached_property
 
 
 class Basket(models.Model):
-
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
+    product = models.ForeignKey(Product, verbose_name='продукт', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
 
     def _get_product_cost(self):
-        "return cost of all products this type"
         return self.product.price * self.quantity
 
     product_cost = property(_get_product_cost)
